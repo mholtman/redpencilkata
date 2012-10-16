32d27a84c537828f4bf3b0235807f46da612a777
Mark Holtman <mark.holtman@abbott.com>
2012-10-16 10:57:22 -0400

    initial checkin with first test

---
+++ RedPencilKata.Domain/RedPencilItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RedPencilKata.Domain
+{
+    public class RedPencilItem
+    {
+        public RedPencilItem(decimal originalPrice)
+        {
+            OriginalPrice = originalPrice;
+        }
+
+        public decimal OriginalPrice { get; private set; }
+    }
+}
---
+++ RedPencilKata.Tests/Domain/RedPencilEntityTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using RedPencilKata.Domain;
+
+
+namespace RedPencilKata.Tests.Domain
+{
+    [TestFixture]
+    public class RedPencilEntityTests
+    {
+
+        [Test]
+        public void can_create_entity_with_price()
+        {
+            RedPencilItem item  = new RedPencilItem(100.00m);
+            Assert.AreEqual(100.00m, item.OriginalPrice);
+        }
+
+    }
+}
