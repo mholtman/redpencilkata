5c100a60691a78300b1c670ca019265617076b5a
Mark Holtman <mark.holtman@abbott.com>
2012-10-16 16:27:59 -0400

    re-wrote rule tests

--- RedPencilKata.Tests/Domain/MarkdownRuleTests.cs
+++ RedPencilKata.Tests/Domain/MarkdownRuleTests.cs
@@ -11,7 +11,7 @@ namespace RedPencilKata.Tests.Domain
     public class MarkdownRuleTests
     {
         [Test]
-        public void upperbound_rule_test()
+        public void upperbound_rule_should_pass_for_30_percent_or_less()
         {
             UpperBoundRule rule = new UpperBoundRule();
             RedPencilItem item = new RedPencilItem(100.00m);
@@ -21,7 +21,29 @@ namespace RedPencilKata.Tests.Domain
         }
 
 
-      
+        [Test]
+        public void uppoerbound_rule_should_fail_for_more_than_30_percent()
+        {
+            RedPencilItem item = new RedPencilItem(100.00m);
+
+            Assert.IsFalse(new UpperBoundRule().Process(item, 69.99m));
+        }
+
+        [Test]
+        public void lowerbound_rule_should_pass_for_5_percent_or_greater()
+        {
+            RedPencilItem item = new RedPencilItem(100.00m);
+            
+            Assert.IsTrue(new LowerBoundRule().Process(item, 95.00m));
+        }
+
+        [Test]
+        public void lowerbound_rule_shoud_fail_for_less_than_5_percent()
+        {
+            RedPencilItem item = new RedPencilItem(100.00m);
+
+            Assert.IsFalse(new LowerBoundRule().Process(item, 96.66m));
+        }
         
     }
 }
